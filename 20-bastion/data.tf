736d70157b1125272b4043e611b5508f2a5b5a32
sreekanthp <varshisree2@gmail.com>
2025-06-24 22:05:34 +0530

    terraform

---
+++ 20-bastion/data.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "joindevops" {
+    owners      = [""]
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name    = "virtualization-type"
+        values  = ["hvm"]
+    }
+}
+
+data "aws_ssm_parameter" "bastion_sg_id" {
+    name = "/${var.project}/${var.environment}/bastion_sg_id"
+}
+
